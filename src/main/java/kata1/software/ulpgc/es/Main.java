b8c9867d2b1a1fa5f1062610f60f374b4c87a2dd
Xx_MARRE_xX <xx_marre_xx@hotmail.com>
2023-12-30 17:32:15 +0000

    Este es mi primer push.

--- src/main/java/kata1/software/ulpgc/es/Main.java
+++ src/main/java/kata1/software/ulpgc/es/Main.java
@@ -6,6 +6,6 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+        System.out.println("Esto es un push!");
     }
 }

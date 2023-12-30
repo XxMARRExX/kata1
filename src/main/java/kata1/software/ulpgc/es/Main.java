d409a0ba1ccd61e4ab0234427bc9ff96a36dcd40
Xx_MARRE_xX <xx_marre_xx@hotmail.com>
2023-12-30 17:28:49 +0000

    Esta es mi primera prueba de la kata 1.

---
+++ src/main/java/kata1/software/ulpgc/es/Main.java
@@ -0,0 +1,11 @@
+package kata1.software.ulpgc.es;
+
+import javax.sound.midi.Soundbank;
+import java.net.SocketOption;
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+}
